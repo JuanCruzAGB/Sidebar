c081f96fc9806836437c2a4bba4f85b2dabde261
Archimak <43325751+JuanCruzAGB@users.noreply.github.com>
2021-03-22 16:38:47 -0300

    New JS logic

--- js/Sidebar.js
+++ js/Sidebar.js
@@ -1,122 +1,63 @@
+// ? JuanCruzAGB repository
+import Class from "../../JuanCruzAGB/Class.js";
+
 /**
  * * Sidebar makes an excellent sidebar.
  * @export
  * @class Sidebar
+ * @author Juan Cruz Armentia <juancarmentia@gmail.com>
+ * @extends Class
  */
-export class Sidebar{
+export class Sidebar extends Class {
     /**
      * * Creates an instance of Sidebar.
-     * @param {object} properties - Sidebar properties.
+     * @param {object} [props] Sidebar properties:
+     * @param {string} [props.id='sidebar-1'] Sidebar primary key.
+     * @param {string} [props.position='left'] Sidebar position.
+     * @param {object} [states] Sidebar states:
+     * @param {boolean} [states.open=false] Sidebar open state.
      * @memberof Sidebar
      */
-    constructor(properties = {
+    constructor (props = {
         id: 'sidebar-1',
         position: 'left',
     }, states = {
         open: false,
-    }){
-        this.setProperties(properties);
-        this.setStates(states);
-        this.setHTML();
-        this.setButtons();
-        this.checkOpenedSidebar();
-    }
-
-    /**
-     * * Set the Sidebar properties.
-     * @param {object} properties - Sidebar properties.
-     * @memberof Sidebar
-     */
-    setProperties(properties = {
-        id: 'sidebar-1',
-        position: 'left',
-    }){
-        this.properties = {};
-        this.setId(properties);
-        this.setPosition(properties);
-    }
-
-    /**
-     * * Set the Sidebar states.
-     * @param {object} states - Sidebar states.
-     * @memberof Sidebar
-     */
-    setStates(states = {
-        open: false,
-    }){
-        this.states = {};
-        this.setOpen(states);
-    }
-
-    /**
-     * * Set the Sidebar ID.
-     * @param {object} properties - Sidebar properties.
-     * @memberof Sidebar
-     */
-    setId(properties = {
-        id: 'left',
-    }){
-        this.properties.id = properties.id;
-    }
-
-    /**
-     * * Set the Sidebar position.
-     * @param {object} properties - Sidebar properties.
-     * @memberof Sidebar
-     */
-    setPosition(properties = {
-        position: 'left',
-    }){
-        this.properties.position = properties.position;
-    }
-
-    /**
-     * * Set the Sidebar open state.
-     * @param {object} states - Sidebar states.
-     * @memberof Sidebar
-     */
-    setOpen(states = {
-        open: [],
-    }){
-        this.states.open = states.open;
-    }
-
-    /**
-     * * Set the Sidebar HTML Element.
-     * @memberof Sidebar
-     */
-    setHTML(){
+    }) {
+        super(props, states);
         let sidebars = document.querySelectorAll('.sidebar');
         if(sidebars.length){
             for(const sidebar of sidebars){
-                if(sidebar.id == this.properties.id){
-                    this.html = sidebar;
+                if(sidebar.id == this.props.id){
+                    this.setHTML(sidebar);
                 }
             }
         }
+        this.setButtons();
+        this.checkOpenedSidebar();
     }
 
     /**
      * * Set the opener & the closer buttons.
      * @memberof Sidebar
      */
-    setButtons(){
+    setButtons () {
         let sidebar = this;
         this.btnsOpener = [];
-        let btnsOpener = document.querySelectorAll(`.sidebar-button.open-btn.${this.properties.position}`);
-        this.btnCloser = document.querySelector(`#${this.properties.id} .sidebar-button.close-btn`);
-        for(const btn of btnsOpener){
-            if(btn.href.split('#').pop() == this.properties.id){
+        let btnsOpener = document.querySelectorAll(`.sidebar-button.open-btn.${ this.props.position }`);
+        this.btnCloser = document.querySelector(`#${ this.props.id } .sidebar-button.close-btn`);
+        for (const btn of btnsOpener) {
+            if (btn.href.split('#').pop() == this.props.id) {
                 this.btnsOpener.push(btn);
-                btn.addEventListener('click', function(e){
-                    if(this.classList.contains('sidebar-button')){
+                btn.addEventListener('click', (e) => {
+                    if(btn.classList.contains('sidebar-button')){
                         e.preventDefault();
                     }
                     sidebar.switch();
                 });
             }
         }
-        this.btnCloser.addEventListener("click", function(e){
+        this.btnCloser.addEventListener("click", (e) => {
             e.preventDefault();
             sidebar.switch();
         });
@@ -127,11 +68,11 @@ export class Sidebar{
      * * Add the close event to the Sidebar custom buttons.
      * @memberof Sidebar
      */
-    addEventToSidebarButtons(){
+    addEventToSidebarButtons () {
         let sidebar = this;
-        let sidebarButtons = document.querySelectorAll(`#${this.properties.id} .sidebar-link.sidebar-button`);
-        for(const btn of sidebarButtons){
-            btn.addEventListener('click', function(e){
+        let sidebarButtons = document.querySelectorAll(`#${ this.props.id } .sidebar-link.sidebar-button`);
+        for (const btn of sidebarButtons) {
+            btn.addEventListener('click', (e) => {
                 sidebar.switch();
             });
         }
@@ -139,11 +80,11 @@ export class Sidebar{
 
     /**
      * * Switch the Sidebar open state.
-     * @returns
+     * @returns {boolean}
      * @memberof Sidebar
      */
-    switch(){
-        switch(this.states.open){
+    switch () {
+        switch (this.states.open) {
             case true:
                 this.close();
                 return false;
@@ -157,9 +98,9 @@ export class Sidebar{
      * * Open the Sidebar
      * @memberof Sidebar
      */
-    open(){
-        this.states.open = true;
-        if(this.html.classList.contains('closed')){
+    open () {
+        this.setStates('open', true);
+        if (this.html.classList.contains('closed')) {
             this.html.classList.remove('closed');
         }
         this.html.classList.add('opened');
@@ -169,9 +110,9 @@ export class Sidebar{
      * * Close the Sidebar
      * @memberof Sidebar
      */
-    close(){
-        this.states.open = false;
-        if(this.html.classList.contains('opened')){
+    close () {
+        this.setStates('open', false);
+        if (this.html.classList.contains('opened')) {
             this.html.classList.remove('opened');
         }
         this.html.classList.add('closed');
@@ -181,31 +122,29 @@ export class Sidebar{
      * * Check if should be a current Dropdown open.
      * @memberof Dropdown
      */
-    checkOpenedSidebar(){
-        if(this.states.open){
+    checkOpenedSidebar () {
+        if (this.states.open) {
             this.switch();
         }
     }
 
-    /**
-     * * Get the Sidebar positions.
-     * @static
-     * @param {object} properties - The Sidebar properties.
-     * @return {object}
-     * @memberof Sidebar
-     */
-    static getPositions(properties = {position: ['left', 'right'],}){
-        if(typeof properties.position != 'object'){
-            throw new Error({
-                status: 415,
-                message: 'The Sidebar properties position must be a valid object.',
-                display: true,
-            });
-        }
-        let positions = [];
-        for(const position of properties.position){
-            positions.push(position);
-        }
-        return positions;
-    }
+    // static getPositions (props = {
+    //     position: ['left', 'right'],
+    // }) {
+    //     if (typeof props.position != 'object') {
+    //         throw new Error({
+    //             status: 415,
+    //             message: 'The Sidebar props position must be a valid object.',
+    //             display: true,
+    //         });
+    //     }
+    //     let positions = [];
+    //     for(const position of props.position){
+    //         positions.push(position);
+    //     }
+    //     return positions;
+    // }
 }
+
+// ? Default export
+export default Sidebar;
